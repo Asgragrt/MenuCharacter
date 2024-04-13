55cd096893ea6dd2a8e3741dcb791e935aa7c3cf
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-13 01:06:38 -0600

    :art: Refactor and clean main patch

--- Managers/ModManager.cs
+++ Managers/ModManager.cs
@@ -1,6 +1,29 @@
-﻿namespace MenuCharacter.Managers;
+﻿using Il2Cpp;
+using Il2CppAssets.Scripts.Database;
+using Il2CppAssets.Scripts.PeroTools.Commons;
+using Il2CppAssets.Scripts.PeroTools.Managers;
+using Il2CppAssets.Scripts.UI.Panels;
+using Il2CppPeroTools2.Resources;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MenuCharacter.Managers;
 
 internal static class ModManager
 {
-    //internal static readonly LoadingExchangeUnlocker LoadingUnlocker = new();
+    internal static GameObject MenuGirl { get; set; }
+
+    internal static void CreateGirl(PnlStage stage)
+    {
+        Object.Destroy(MenuGirl);
+
+        var assetName = Singleton<ConfigManager>.instance
+            .GetConfigObject<DBConfigCharacter>()
+            .GetCharacterInfoByIndex(DataHelper.selectedRoleIndex)
+            .victoryShow;
+
+        MenuGirl = ResourcesManager.instance.LoadFromName<GameObject>(assetName).FastInstantiate(stage.transform);
+        MenuGirl.transform.position = new Vector3(-6.7f, -5f, 1f);
+        MenuGirl.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+    }
 }
--- Patches/StagePatch.cs
+++ Patches/StagePatch.cs
@@ -1,16 +1,6 @@
-using System.Runtime.CompilerServices;
 using HarmonyLib;
-using Il2Cpp;
-using Il2CppAssets.Scripts.Database;
-using Il2CppAssets.Scripts.PeroTools.Commons;
-using Il2CppAssets.Scripts.UI;
 using Il2CppAssets.Scripts.UI.Panels;
-using Il2CppPeroTools2.Resources;
-using Il2CppSirenix.OdinInspector.Demos;
-using MelonLoader;
-using UnityEngine;
-using ConfigManager = Il2CppAssets.Scripts.PeroTools.Managers.ConfigManager;
-using Type = Il2CppSystem.Type;
+using MenuCharacter.Managers;
 
 namespace MenuCharacter.Patches;
 
@@ -20,17 +10,6 @@ internal static class StagePatch
     [HarmonyPostfix]
     internal static void Postfix(PnlStage __instance)
     {
-        var a = Singleton<ConfigManager>.instance
-            .GetConfigObject<DBConfigCharacter>()
-            .GetCharacterInfoByIndex(DataHelper.selectedRoleIndex)
-            .victoryShow;
-        Melon<Main>.Logger.Msg(a);
-
-        //__instance.gameObject.AddComponent<MuseShow>();
-
-        var b = ResourcesManager.instance.LoadFromName<GameObject>(a).FastInstantiate(__instance.transform);
-        b.transform.position = new Vector3(-6.7f, -5f, 1f);
-        b.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
-        //b.AddComponent<MuseShow>().Init();
+        ModManager.CreateGirl(__instance);
     }
 }
