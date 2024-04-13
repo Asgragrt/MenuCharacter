6e6ec009e26d73ac7b6cee3565dcc15341f84ab7
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-13 02:19:52 -0600

    :sparkles: Update position when changing menu

--- Managers/ModManager.cs
+++ Managers/ModManager.cs
@@ -1,9 +1,11 @@
-﻿using Il2Cpp;
+﻿using System.Collections;
+using Il2Cpp;
 using Il2CppAssets.Scripts.Database;
 using Il2CppAssets.Scripts.PeroTools.Commons;
 using Il2CppAssets.Scripts.PeroTools.Managers;
 using Il2CppAssets.Scripts.UI.Panels;
 using Il2CppPeroTools2.Resources;
+using MelonLoader;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -14,6 +16,8 @@ internal static class ModManager
     internal static GameObject MenuGirl { get; set; }
 
     internal static PnlStage PnlStage { get; set; }
+    
+    internal static PnlPreparation PnlPreparation { get; set; }
 
     internal static void CreateGirl()
     {
@@ -30,8 +34,37 @@ internal static class ModManager
             .LoadFromName<GameObject>(assetName)
             .FastInstantiate(PnlStage.transform);
 
+        SetGirlStageParent();
+
         MenuGirl.name = "MenuGirlObject";
         MenuGirl.transform.position = new Vector3(6.7f, -5f, 1f);
         MenuGirl.transform.localScale = new Vector3(-0.5f, 0.5f, 1f);
+    }
+
+    internal static void UpdateGirlParent(bool isActive)
+    {
+        MelonLoader.Melon<Main>.Logger.Msg("Updating...");
+        if (!PnlPreparation || !MenuGirl) return;
+
+        if (isActive)
+        {
+            MenuGirl.transform.SetParent(PnlPreparation.transform);
+            PnlPreparation.transform.Find("Start").SetAsLastSibling();
+            return;
+        }
+
+        MelonCoroutines.Start(SetGirlStageParentCoroutine());
+    }
+
+    private static void SetGirlStageParent()
+    {
+        //MenuGirl.transform.SetParent(PnlStage.transform.Find("StageUi").Find("Info"));
+        MenuGirl.transform.SetParent(PnlStage.transform);
+    }
+
+    private static IEnumerator SetGirlStageParentCoroutine()
+    {
+        yield return null;
+        SetGirlStageParent();
     }
 }
---
+++ Patches/PreparationPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppAssets.Scripts.UI.Specials;
+using MenuCharacter.Managers;
+
+namespace MenuCharacter.Patches;
+
+[HarmonyPatch]
+internal static class PreparationPatch
+{
+    [HarmonyPatch(typeof(PnlPreparation), nameof(PnlPreparation.Awake))]
+    [HarmonyPostfix]
+    internal static void AwakePostfix(PnlPreparation __instance)
+    {
+        ModManager.PnlPreparation = __instance;
+    }
+    
+    [HarmonyPatch(typeof(AutoPushPopPanel), nameof(AutoPushPopPanel.OnPnlPreparationActiveChanged))]
+    [HarmonyPrefix]
+    internal static void OnEnablePostfix(bool active)
+    {
+        ModManager.UpdateGirlParent(active);
+    }
+}
