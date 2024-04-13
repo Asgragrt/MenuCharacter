a49a624e25a8957fa63f3ff7a9d3c370d0163469
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-13 01:33:02 -0600

    :sparkles: Update girl when changing character

---
+++ Patches/CharacterChangePatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Il2CppAssets.Scripts.Database;
+using MenuCharacter.Managers;
+
+namespace MenuCharacter.Patches;
+
+[HarmonyPatch(typeof(DataHelper), nameof(DataHelper.selectedRoleIndex), MethodType.Setter)]
+internal static class CharacterChangePatch
+{
+    internal static void Postfix()
+    {
+        ModManager.CreateGirl();
+    }
+}
