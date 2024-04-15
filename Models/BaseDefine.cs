0dc06053fe20465dfe17e130e02e306aa5d6b138
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-14 21:41:21 -0600

    :bug: Fixed default value of string to index

--- Models/BaseDefine.cs
+++ Models/BaseDefine.cs
@@ -7,7 +7,7 @@ internal abstract class BaseDefine<T>(T defVal) : IDefine where T : struct, Enum
     string IDefine.IndexToString(int i) => Enum.GetName(typeof(T), i) ?? ((IDefine)this).Default;
 
     int IDefine.StringToIndex(string s) =>
-        Enum.TryParse(s, true, out T result) ? result.ToInt32(null) : default(T).ToInt32(null);
+        Enum.TryParse(s, true, out T result) ? result.ToInt32(null) : defVal.ToInt32(null);
 
     string IDefine.Options() => string.Join("\n", Enum.GetNames<T>());
 }
